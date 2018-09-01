c0b554f0461d9ba8140f2ecb8112d23c86081925
neil patel <neil@myindianweddingapp.com>
2018-09-01 11:02:19 +0100

    first commit

---
+++ lambda/custom/index.js
@@ -0,0 +1,47 @@
+var Alexa = require('alexa-sdk');
+
+exports.handler = function(event, context, callback) {
+  var alexa = Alexa.handler(event, context);
+
+  Alexa.dynamoTable = 'testGOT';
+
+  alexa.registerHandlers(handlers);
+  alexa.execute();
+
+};
+
+var handlers = {
+  'LaunchRequest': function() {
+      this.emit('WelcomeIntent');
+  },
+  'WelcomeIntent': function() {
+      this.emit(':ask', 'Welcome to the Game of Thrones Characters Skill! Please pick a random number from one to seven hundred to select a character');
+  },
+  'MyIntent': function() {
+      var randomNumber = this.event.request.intent.slots.number.value;
+      var myRequest = parseInt(randomNumber);
+
+      httpGet (myRequest, (myResult) => {
+        console.log("sent : " + myRequest);
+        console.log("received : " + myResult);
+
+        this.emit(':tell', 'This Game of Thrones character is ${myResult.name} which also has the title od ${myResult.titles}');
+        this.attributes['character_name'] = myResult.name;
+        this.attributes['character_title'] = myResult.titles;
+        this.emit(':responseReady');
+      }
+    };
+  }
+};
+
+var https = require('https');
+
+function httpsGet (myData, callback) {
+
+  var options = {
+    host: 'anapioficeandfire.com',
+    post 443,
+    path: '/api/characters/' + encodeURIComponent(myData),
+    method: 'GET',
+  };
+}
